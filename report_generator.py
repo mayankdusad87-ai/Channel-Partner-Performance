521c828b5809713dfc256b3adef1d59126a25220
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-04-23 16:45:34 +0530

    Update report_generator.py

--- report_generator.py
+++ report_generator.py
@@ -6,14 +6,26 @@ def create_ppt(insights, summary, monthly):
     prs = Presentation()
 
     slide = prs.slides.add_slide(prs.slide_layouts[0])
-    slide.shapes.title.text = "Channel Partner Performance Report"
+    slide.shapes.title.text = "CP Performance Strategy"
     slide.placeholders[1].text = str(datetime.now().date())
 
     slide = prs.slides.add_slide(prs.slide_layouts[1])
     slide.shapes.title.text = "Key Insights"
     slide.placeholders[1].text = insights
 
-    file = "report.pptx"
+    top = summary.sort_values(by="Conversion %", ascending=False).head(5)
+
+    slide = prs.slides.add_slide(prs.slide_layouts[1])
+    slide.shapes.title.text = "Top CPs"
+    slide.placeholders[1].text = top.to_string(index=False)
+
+    risk = summary[summary["Conversion %"] < 5]
+
+    slide = prs.slides.add_slide(prs.slide_layouts[1])
+    slide.shapes.title.text = "Risk CPs"
+    slide.placeholders[1].text = risk.to_string(index=False)
+
+    file = "Strategy_Report.pptx"
     prs.save(file)
 
     return file
