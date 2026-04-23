6605f05c8350fab8b3b5cbc813776b6cc10bff46
mayankdusad87-ai <mayankdusad87@gmail.com>
2026-04-23 15:51:07 +0530

    Update insights_engine.py

--- insights_engine.py
+++ insights_engine.py
@@ -5,19 +5,19 @@ def generate_insights(summary, monthly, active_cp):
     api_key = os.getenv("GROQ_API_KEY")
 
     if not api_key:
-        return "⚠️ Groq API key not configured. Please add it in Streamlit Secrets."
+        return "⚠️ Please add GROQ_API_KEY in Streamlit Secrets"
 
     client = Groq(api_key=api_key)
 
     prompt = f"""
-You are a real estate sourcing performance analyst.
+You are a senior real estate strategist having 20 years work experienc in Lodha, Rustomjee etc..
 
 Analyze the data below:
 
-OVERALL PERFORMANCE:
+OVERALL:
 {summary.to_string(index=False)}
 
-MONTHLY PERFORMANCE:
+MONTHLY:
 {monthly.to_string(index=False)}
 
 Active CPs (last 30 days): {active_cp}
@@ -26,15 +26,16 @@ Give:
 1. Key insights
 2. Risks
 3. Broker behavior patterns
-4. Actionable recommendations
+4. Clear actionable recommendations
 
-Be sharp, concise, and business-focused.
+Use numbers and percentages.
+Keep it sharp and business-focused.
 """
 
     response = client.chat.completions.create(
         model="llama3-70b-8192",
         messages=[
-            {"role": "system", "content": "You are a senior real estate business analyst."},
+            {"role": "system", "content": "You are a business analyst."},
             {"role": "user", "content": prompt}
         ],
         temperature=0.3
